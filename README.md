56e5f1dffb0d95148a676e684bd3f00ef3276eda
Femtox <research@femtox.de>
2015-06-02 18:00:30 +0200

    uptube.sh upload

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # uptube
 simple bash bot which converts audio files to video and uploads them to youtube
+
+# HowTo
+
+* Start the script from within the directory where the subject audio files exist.
+* Inside that direcory there must live one background image bg.jpg with a resolution
+	of 1920x1080 and one foreground image fg.jpg with 1080x1080. If they have another
+	resolution they will get resized.
+* The script asks for your youtube login information and upload related stuff.
+* With the "searchterms" you provide, it will create a list of keywords to apply
+	on the video upload.
+* A playlist with the directory name will get created and all videos will be
+	queued in it.
+* The description for the videos must be in a file called ripping.log
+	(That is the standard log file of the rubyripper, but any content will do it,
+	except some special chars which interfere with the youtube-upload internals)
+
+Thats about it.
+
+# Dependencies
+youtube-upload, cewl, lynx, ffmpeg, rm, sed, echo, awk, tr, cat
+
+# If you spot one or more mistakes i made, please let me know about it,
+# but for me it works so far.
---
+++ uptube.sh
@@ -0,0 +1,81 @@
+#!/bin/bash
+# Author: Femtox - research at femtox dot de
+# License: MIT
+# simple bash bot which converts audio files to video and uploads them to youtube
+
+infrmtion() {
+	read -p "youtube email or username: " usrnme
+	read -p "youtube password: " psswrd
+	read -p "some searchterms for keyword generation (seperated by whitespace): " srchtrm
+	echo -e "\ncategories: Tech Education Animals People Travel Entertainment Howto Sports Autos Music News Games Nonprofit Comedy Film"
+	read -p "category: " ctgry
+	read -p "keep video files after upload? y/n: " kvf
+}
+
+# keyword generation
+kw() {
+	srchtrm=$(echo $srchtrm | sed  -e  's/\ /%20/g')
+	lynx -dump 'http://www.google.com/search?q='${srchtrm}'&num=50' | grep -avi 'google\|youtube\|\.\.\.' | \
+		grep -ai www | awk '{print $1}' > links.data
+	i=1
+	while [[ $i -ne 9 ]]
+	do
+		search=`awk NR==$i links.data`
+		cewl -d0 -c -w tmp${i}.data $search
+		((i++))
+	done
+	cat tmp*.data > keywords.data; rm tmp*.data links.data
+	awk '{print $2 " " $1}' keywords.data | sort -rh | awk '{print $2}' > keywords.tmp
+	cat keywords.tmp | tr [:upper:] [:lower:] > keywords.data
+	awk '!seen[$0]++' keywords.data > keywords.tmp
+	tr -d '\n' < keywords.tmp > keywords.data
+	cut -c-419 keywords.data > keywords.tmp
+	sed 's/\w*[^,]$//' keywords.tmp > keywords.data
+	keywords=`cat keywords.data`
+	rm keywords.data keywords.tmp
+}
+
+# convert to video and upload
+caup() {
+	ffmpeg -i bg.jpg -vf scale=1920:1080 bghd.jpg
+	ffmpeg -i fg.jpg -vf scale=1080:1080 fghd.jpg
+	i=1
+	sed 's/>//g' ripping.log
+	SAVEIFS=$IFS
+	IFS=$(echo -en "\n\b")
+	for audiofile in $(ls *.{m4a,mp3,flac,wav,ogg} 2> /dev/null ) #| sed 's/ /\\ /g')
+	do
+		if [[ $(echo $audiofile | awk -F . '{print $NF}') == flac ]]
+		then
+			ffmpeg  -loop 1 -framerate 1 -i bghd.jpg -i fghd.jpg -i $audiofile -shortest -filter_complex "overlay=(W-w)/2:,format=yuv420p" \
+				-c:v libx264 -r 18 -preset slower -tune stillimage -c:a aac -strict -2 -movflags +faststart ${audiofile%.*}.mp4
+		else
+			ffmpeg  -loop 1 -framerate 1 -i bghd.jpg -i fghd.jpg -i $audiofile -shortest -filter_complex "overlay=(W-w)/2:,format=yuv420p" \
+				-c:v libx264 -r 18 -preset slower -tune stillimage -c:a copy -movflags +faststart ${audiofile%.*}.mp4
+		fi
+		if [[ $i == 1 ]]
+		then
+			playlst=$(youtube-upload -m $usrnme -p $psswrd --create-playlist=$(echo "'`pwd`'" | xargs basename) | sed 's/https/http/g')
+		fi
+		lnk=$(youtube-upload -m $usrnme -p $psswrd -c $ctgry -t ${audiofile%.*} --description="$(< \ripping.log)"\
+			--keywords $keywords ~/stuff/temp/${audiofile%.*}.mp4)
+		youtube-upload -m $usrnme -p $psswrd  --add-to-playlist=$playlst $lnk
+		if [[ $kvf == n || $kvf == N || $kvf == no || $kvf == No || $kvf == NO ]]
+		then
+			rm ${audiofile%.*}.mp4
+		fi
+		((i++))
+	done
+	rm bghd.jpg fghd.jpg
+	IFS=$SAVEIFS
+}
+
+main() {
+	infrmtion
+	kw
+	caup
+	exit 0
+}
+
+main
+exit 1
